21dad11a16ba35f4be6cdf0ebeb7cb99effc127a
jieun <jieun98.dev@gmail.com>
2023-08-04 16:53:04 +0900

    [add] day01.js

---
+++ JS01/js/day01.js
@@ -0,0 +1,53 @@
+console.log('hi');
+console.log("aaa");
+
+/*
+    1. 변수 선언 let, var
+    상수: const
+*/
+const irum='hong girl dong'
+console.log('name', irum);
+// irum ='park!!';
+// console.log('new name', irum);
+
+// var a = 10;
+console.log('a', a);
+var a= 20;
+console.log('a', a);
+
+console.log("===========================");
+
+let su;
+su=10;
+console.log(su);
+
+/*
+    2. type
+*/
+
+let a1=10;
+let b='aaa';
+let c=10.2;
+let d=10==1000;
+let f=null;
+let e;
+
+console.log(typeof a1, 'a1');
+console.log(typeof b, 'b');
+console.log(typeof c, 'c');
+console.log(typeof d, 'd');
+console.log(typeof f, 'f');
+console.log(typeof e, 'e');
+console.log(typeof 10/0, '10/1');
+console.log(typeof -10/0, '-10/1');
+console.log(typeof 'aa'/0, 'aa/1');
+
+let t1=[]; //배열
+// let t2={}; //객체
+// let t3=function(){
+//     console.log('hello');
+// }; //함수 function
+
+console.log(typeof t1);
+// console.log(typeof t2);
+// console.log(typeof t3);
