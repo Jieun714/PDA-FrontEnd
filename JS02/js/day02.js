8404681c8b8f9bf1a4d83cde741333686913b7dc
jieun <jieun98.dev@gmail.com>
2023-08-07 08:48:53 +0900

    [add] javascript

---
+++ JS02/js/day02.js
@@ -0,0 +1,68 @@
+"use strict"
+/**
+ * let, var, const
+ */
+
+//var a=undefined
+// console.log('a', a);
+
+let a =30;
+{
+    let a=10;
+    console.log('block a', a);
+}
+console.log('global a', a);
+
+
+var su =30;
+{
+    var su=10;
+    console.log('block su', su);
+}
+console.log('global su', su);
+
+console.log('----------------------------');
+
+for(let i=1; i<=5; i++) {
+    console.log('i' , i);
+}
+// console.log('i', i);
+
+console.log('----------------------------');
+
+let tf='';
+if(tf){
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+console.log('----------------------------');
+
+let arr=[10, 20, 30] //배열 
+
+let fn=function() {
+    console.log('function fn[]');  
+}
+fn();
+console.log('arr.length', arr.length);
+
+for(let i=0; i<arr.length; i++){
+    console.log('arr[i]' ,arr[i]);
+}
+
+console.log('----------------------------');
+
+console.log(typeof arr);
+console.log(typeof (arr));
+console.log(typeof fn);
+console.log(typeof (fn));
+
+console.log('----------------------------');
+
+let person ={};
+console.log(typeof person);
+console.log(Array.isArray(arr));
+console.log(Array.isArray(person));
+
+console.log('----------------------------');
