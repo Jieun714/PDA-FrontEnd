b4f12e123a8f778518108814a2549aaa007b9328
jieun <jieun98.dev@gmail.com>
2023-08-07 09:44:01 +0900

    [add] 비교연산자

--- JS02/js/day02.js
+++ JS02/js/day02.js
@@ -77,4 +77,76 @@ console.log(typeof ('a'/2), ('a')/2);
 
 console.log('----------------------------');
 
+console.log(Boolean(''), Boolean(' '));
+console.log(Boolean(0), Boolean('0'));
+console.log(Number('10'), Number('aa'), Number(null));
+Number(undefined);
+console.log(String(10));
+
+console.log('----------------------------');
+
+let name ='hong gil dong';
+let jumsu = 100;
+console.log('name :', name, ', jumsu :', jumsu);
+console.log(`name : ${name}, jumsu : ${jumsu}`)
+
+console.log('----------------------------');
+
+/*
+단항연산자
+-> ++(증감연산자), --(감소연산자)
+이항연산자
+-> 산술연산자 : +,  -,  *,   /,   %,  ** 
+
+ */
+
+let a1 = 10;
+let b = a1++;
+console.log(a1, b);
+
+let t1=10;
+let t2 = 3;
+console.log(t1+t2);
+console.log(t1*t2);
+console.log(t1/t2);
+console.log(t1%t2);
+console.log(t1**t2);
+
+console.log(10+20+'aaa');
+console.log('aaa'+10+20);
+
+console.log('----------------------------');
+
+/* 비교연산 */
+console.log(10>=10);
+
+let a2 ='10';
+let b2 = 10;
+console.log(typeof a2, typeof b2);
+console.log(a==b);
+console.log(a===b);
+
+console.log('----------------------------');
+
+let m1={name: 'hong', age:20};
+let m2 = m1;
+let m3 ={name:'hong', age:20};
+
+console.log(typeof m1, typeof m2, typeof m3);
+console.log(m1==m2, m1===m2); //ture
+console.log(m1==m3, m1===m3); //false
+
+console.log('----------------------------');
+
+console.log(null==undefined); //true
+console.log(null===undefined); //false
+console.log(false==''); //ture
+console.log(false==0); //true
+console.log(false===0); //false
+
+console.log('----------------------------');
+
+
+
+console.log('----------------------------');
 
