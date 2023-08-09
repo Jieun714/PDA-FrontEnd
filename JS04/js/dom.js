6631d85d3328c1b7e13362e28ea083a3cc4b1314
jieun <jieun98.dev@gmail.com>
2023-08-09 15:18:46 +0900

    [add] dom 관련

--- JS04/js/dom.js
+++ JS04/js/dom.js
@@ -18,5 +18,14 @@ console.log(li_class);
 let tagName = document.getElementsByTagName('li');
 console.log(tagName);
 
+// listName이 들어간 element 이름
 let names = document.getElementsByName('listName');
 console.log(names);
+
+// body의 자손들
+let child1 = document.body.children;
+console.log(child1);
+
+// 세 번째 자료 선택
+let lis = document.getElementsByTagName('li')[2].className;
+console.log(lis);
