998bd9dbb9a156b221215abce33ccc69a9ae5479
jieun <jieun98.dev@gmail.com>
2023-08-09 09:25:25 +0900

    [add] 내장객체 String

--- JS04/js/day04.js
+++ JS04/js/day04.js
@@ -37,7 +37,36 @@ console.log(p1);
 
 console.log("==========================");
 
+/* Object.is 사용  */
+let o1 = [10, 20, 30];
+let o2 = [10, 20, 30];
 
+console.log(o1===o2); //false
 
+let tf = Object.is(o1[0], o2[0]);
+console.log(tf); //ture
 
 console.log("==========================");
+
+/* String */
+
+/*
+이메일을 입력받아 ID와 도메인 구분
+아이디: aaa
+도메인: aaa.com
+*/
+
+/* split 사용 */
+let email = 'aaa@aaa.com';
+let [id, domain] = email.split('@');
+console.log(`id: ${id}, domain: ${domain}`);
+
+/* 배열로 담아 */
+let result = email.split('@');
+console.log(`id: ${result[0]}, domain: ${result[1]}`);
+
+/* indexOf와 slice 사용  */
+let idx = email.indexOf('@'); //인덱스 추출
+let id2 = email.slice(0, idx);
+let domain2 = email.slice(idx+1, email.length);
+console.log(`id: ${id2}, domain: ${domain2}`);
