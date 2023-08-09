ca45ef8b6ad8a4353140d5fb91ca7fe0944c0092
jieun <jieun98.dev@gmail.com>
2023-08-09 14:17:51 +0900

    [add] dom test

---
+++ JS04/js/dom.js
@@ -0,0 +1,22 @@
+/*
+document.getElementById('win_open').onclick=function(){
+    // window.open('hello');
+    // location.href ="wwww.naver.com";
+    document.write('hello');
+}
+*/
+
+let d = document.body;
+console.log(d);
+
+let list_id = document.getElementById('listId');
+console.log(list_id);
+
+let li_class = document.getElementsByClassName('listClass');
+console.log(li_class);
+
+let tagName = document.getElementsByTagName('li');
+console.log(tagName);
+
+let names = document.getElementsByName('listName');
+console.log(names);
