21bbda397f56d90fe17bf51be2e123629fe47d97
jieun <jieun98.dev@gmail.com>
2023-08-10 09:37:19 +0900

    [add] toggle test

---
+++ JS05/js/dom2.js
@@ -0,0 +1,9 @@
+
+let ele_tr = document.querySelectorAll("tbody tr");
+
+for(let item of ele_tr){
+    item.addEventListener('click', function(){
+        console.log('click : ' + item.textContent);
+        item.classList.toggle("yellow");
+    });
+}
