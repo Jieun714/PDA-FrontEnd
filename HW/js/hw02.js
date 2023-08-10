f93181219adcbfbf083485681b4cb353cf1fd97d
jieun <jieun98.dev@gmail.com>
2023-08-10 17:57:39 +0900

    [add] final

--- HW/js/hw02.js
+++ HW/js/hw02.js
@@ -1,3 +1,9 @@
+"use strict"
+
+/* 자주 사용하는 내용을 글로벌변수로 사용 */
+let leftBox = document.getElementById('leftBox');
+let rightBox = document.getElementById('rightBox');
+
 /* 목록 추가 - 버튼 */
 document.getElementById('addBtn').addEventListener('click', function(){
     appendList();
@@ -12,12 +18,12 @@ function enterKeyPress(e){
 
 /* 왼쪽 상자에 목록을 추가하는 함수 */
 function appendList(){
-    let irum = document.getElementById('irum'); //입력 받은 이름
-    let option = document.createElement('option'); //option 태그
-    let txt = document.createTextNode(irum.value);
-    option.appendChild(txt); //option 태그에 이름 추가
+    const irum = document.getElementById('irum'); //입력 받은 이름
+    if(irum.value == "") return; //공백이면 입력이 안되도록
 
-    let leftBox = document.getElementById('leftBox');
+    const option = document.createElement('option'); //option 태그
+    const txt = document.createTextNode(irum.value);
+    option.appendChild(txt); //option 태그에 이름 추가
     leftBox.appendChild(option); //왼쪽 상자에 값 추가
 
     irum.value = ''; //값을 비워줌
@@ -26,10 +32,7 @@ function appendList(){
 
 /* 왼쪽 상자에서 오른쪽 상자로 이동 */
 document.getElementById('leftToRightBtn').addEventListener('click', function(){
-    // let leftBox = document.querySelectorAll('#leftBox option');
-    let leftBox = document.getElementById('leftBox');
-    let rightBox = document.getElementById('rightBox');
-    let collection = leftBox.selectedOptions; //선택된 요소 확인
+    const collection = leftBox.selectedOptions; //선택된 요소 확인
 
     for(let i=0; i < collection.length; i++){
         console.log(`go rightBox : ${collection[i].value}`); //넘어가는 값 확인
@@ -45,9 +48,7 @@ document.getElementById('leftToRightBtn').addEventListener('click', function(){
 
 /* 오른쪽 상자에서 왼쪽 상자로 이동 */
 document.getElementById('rightToLeftBtn').addEventListener('click', function(){
-    let leftBox = document.getElementById('leftBox');
-    let rightBox = document.getElementById('rightBox');
-    let collection = rightBox.selectedOptions; //선택된 요소 확인
+    const collection = rightBox.selectedOptions; //선택된 요소 확인
 
     for(let i=0; i < collection.length; i++){
         console.log(`go leftBox : ${collection[i].value}`);
@@ -61,14 +62,16 @@ document.getElementById('rightToLeftBtn').addEventListener('click', function(){
     }
 });
 
-/* 클릭시 삭제 */
-const result = document.querySelector('leftBox');
-console.log("result: " + result);
-/*
-result.addEventListener('dblclick', function(e){
-    //부모의 자식을 찾아야함
-    console.log('들어오나?');
-    let parent = e.target.parentElement;
-    parent.removeChild(e.target);
+/* 클릭시 삭제 - 왼쪽 상자 */
+leftBox.addEventListener('dblclick', function(e){
+    console.log(`delete element: ${e.target.value}`); //삭제할 요소 확인
+    const parent = e.target.parentElement;
+    parent.removeChild(e.target); //하위 요소 삭제
 });
-*/
+
+/* 클릭시 삭제 - 오른쪽 상자 */
+rightBox.addEventListener('dblclick', function(e){
+    console.log(`delete element: ${e.target.value}`); //삭제할 요소 확인
+    const parent = e.target.parentElement;
+    parent.removeChild(e.target); //하위 요소 삭제
+});
